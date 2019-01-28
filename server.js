54bfa83e740e44228e138e01ae681ffe0bdbf4fe
Shawn Fiske <durah_shar@hotmail.com>
2019-01-28 13:57:52 -0800

    updated html

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 // For the database
-//var sqlite3 = require('sqlite3');
-//var db = new sqlite3.Database('./db/dnd35.db');
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('./db/dnd35.db');
  
 // Add restful controller
-//require('./routes/ClassController')(app, db, jsonParser);
+require('./routes/ClassController')(app, db, jsonParser);
  
 // Serve static files
-app.use(express.static('wwwroot'))
+app.use(express.static(__dirname + "/wwwroot"));
 
 var port = process.env.PORT || 5500;
 
